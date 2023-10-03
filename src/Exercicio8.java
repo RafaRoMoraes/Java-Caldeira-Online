50e23ef525838922d796d69731e057601eaefa99
Rafael Rodrigues Moraes <135690957+RafaRoMoraes@users.noreply.github.com>
2023-10-02 21:42:58 -0300

    Correção da atividade 8

--- src/Exercicio8.java
+++ src/Exercicio8.java
@@ -2,17 +2,37 @@ import java.util.Scanner;
 
 public class Exercicio8 {
     public static void main(String[] args){
-        String op;
+        int op;
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Informe se você é PCD, gestante, idoso: ");
-        op = s.nextLine().toLowerCase();
-        //leitor e conversor para caixa baixa
+        //menu
+        System.out.println("--Sistema de fila--");
+        System.out.println("1-PCD(Pessoa com Deficiência)");
+        System.out.println("2-Gestante");
+        System.out.println("3-Idoso(a)");
+        System.out.println("4-Nenhuma das anteriores");
+        op = s.nextInt();
 
-        if(op.contains("pcd") || op.contains("gestante") || op.contains("idos")){
-            System.out.println("Você tem direito à fila preferencial!");
-        }else{
-            System.out.println("Você pertence à fila normal!");
+        while(op<1 || op>4){
+            //validação para caso dado seja diferente dos válidos
+            System.out.println("Dado incorreto");
+            System.out.println("--Sistema de fila--");
+            System.out.println("1-PCD(Pessoa com Deficiência)");
+            System.out.println("2-Gestante");
+            System.out.println("3-Idoso(a)");
+            System.out.println("4-Nenhuma das anteriores");
+            op = s.nextInt();
+        }
+
+        switch (op){
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Você tem direito a fila preferencial!");
+                break;
+            default:
+                System.out.println("Você não tem direito a fila preferencial!");
+                break;
         }
     }
 }
